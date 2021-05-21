0e914c4ec796752eaa6aa99fdbd88ae6b43433d1
cep104 <candicelizabethpeters@gmail.com>
2021-05-20 19:58:17 -0500

    crud

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -18,21 +18,33 @@ class MovieController < ApplicationController
 
   
     get "/movies/:id/edit" do 
+        
+        @movie = Movie.find(params[:id])
+        erb :'movies/edit'
         #editing a specific movie
     end
 
     post "/movies" do 
-        @movie = Movie.new(title: params[:title])
-        binding.pry
+        @movie = Movie.new(params)
+        #  binding.pry
         @movie.save
+        
+        redirect :'/movies'
         #create new movies
     end
 
     patch "/movies/:id" do 
+       @movie = Movie.find(params[:id])
+       @movie.update(params["movie"])
+        redirect :"movies/#{@movie.id}"
         #editing a spefic movie
     end
 
     delete "/movies/:id" do 
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        # binding.pry
+        redirect :"/movies"
         #deleting one spefic movie
     end
 
